60b3c01f3b021451201efb4b0bc976ea9578b26c
Solomon Achilles <solohag92@gmail.com>
2022-04-05 15:35:14 +0000

    Update style.css

--- modules/add.js
+++ modules/add.js
@@ -1,18 +1,18 @@
 const scores = [
   {
-    name: "Solomon A.",
+    name: "Solomon A",
     score: 50
   },
   {
-    name: "Sanson K.",
+    name: "Sanson K",
     score: 70
   },
   {
-    name: "Franca B.",
+    name: "Franca B",
     score: 60
   },
   {
-    name: "Edina G.",
+    name: "Edina G",
     score: 50
   }
 ];
