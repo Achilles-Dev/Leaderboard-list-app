a56268dd092a66379bc4847e3115c0d16bfa86e3
Solomon Achilles <solohag92@gmail.com>
2022-04-05 15:21:58 +0000

    Update javascript file | Add new module

---
+++ modules/add.js
@@ -0,0 +1,30 @@
+const scores = [
+  {
+    name: "Solomon A.",
+    score: 50
+  },
+  {
+    name: "Sanson K.",
+    score: 70
+  },
+  {
+    name: "Franca B.",
+    score: 60
+  },
+  {
+    name: "Edina G.",
+    score: 50
+  }
+];
+
+const lists = document.querySelector('.left-section .bottom');
+
+export const addScores = () => {
+  scores.forEach((score) => {
+    let list = document.createElement('li');
+    list.className = 'list-item';
+    list.textContent = `${score.name}: ${score.score}`;
+    lists.appendChild(list);
+  });  
+}
+
